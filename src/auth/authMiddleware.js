9186eafbf486c446fb7d1bb62166dc8ee5eca82a
Andreas Ambühl <36681000+andiswiss@users.noreply.github.com>
2023-03-15 15:40:45 +0100

    singleton variant of enforcer initialization

--- src/auth/authMiddleware.js
+++ src/auth/authMiddleware.js
@@ -1,6 +1,31 @@
 const { newEnforcer } = require('casbin')
+
+/**
+ * Async singleton initialization of the enforcer
+ * @returns {Promise<*>} enforcer
+ */
+const initEnforcer = async () => {
+  if (!initEnforcer.enforcer) {
+    initEnforcer.enforcer = await newEnforcer('src/auth/model.conf', 'src/auth/policy.csv');
+    console.log('Enforcer initialized');
+  }
+  return initEnforcer.enforcer;
+}
+
+/**
+ * Middleware for checking authorization
+ * @param req request
+ * @param res response
+ * @param next next middleware
+ */
 const authMiddleware = async (req, res, next) => {
-  const enforcer = await newEnforcer('./src/auth/model.conf', './src/auth/policy.csv');
+  // TODO: discuss if we should use non-singleton initialization??
+
+  // non-singleton initialization of the enforcer
+  // const enforcer = await newEnforcer('./src/auth/model.conf', './src/auth/policy.csv');
+
+  // singleton initialization of the enforcer
+  const enforcer = await initEnforcer();
 
   const user = req.get('user-group');
   const path = req.path;
