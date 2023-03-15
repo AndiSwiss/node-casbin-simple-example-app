cca6e08b8b56fafd138a617a0e3c38df6bb8d082
Andreas Ambühl <36681000+andiswiss@users.noreply.github.com>
2023-03-15 17:47:26 +0100

    hacky first solution of handling all policies in db

--- src/authMiddleware.js
+++ src/authMiddleware.js
@@ -1,4 +1,12 @@
 const { newEnforcer } = require('casbin')
+const { MongoAdapter } = require('casbin-mongodb-adapter')
+
+require('dotenv').config()
+
+const pw = process.env.MONGODB_PW;
+const dbUser = process.env.MONGODB_USER;
+const mongoDbCluster = process.env.MONGODB_CLUSTER;
+const dbUrl = `mongodb+srv://${dbUser}:${pw}@${mongoDbCluster}/casbinExampleApp?retryWrites=true&w=majority`;
 
 /**
  * Async singleton initialization of the enforcer
@@ -6,7 +14,58 @@ const { newEnforcer } = require('casbin')
  */
 const initEnforcer = async () => {
   if (!initEnforcer.enforcer) {
-    initEnforcer.enforcer = await newEnforcer('./src/auth/model.conf', './src/auth/policy.csv');
+
+    // dbUrl, 'casbinExampleApp', 'casbinRules'
+    const adapter = await MongoAdapter.newAdapter({
+      uri: dbUrl,
+      collection: 'casbinRules',
+      database: 'casbinExampleApp',
+    });
+
+    const e = await newEnforcer('./src/auth/model.conf', adapter);
+
+
+    await e.loadPolicy();
+    /**
+     # Routes
+     # ======
+     p, user, /allowlist, GET
+     p, user, /users, GET
+     p, user, /users/info, GET
+
+     p, poweruser, /allowlist, POST
+     p, poweruser, /allowlist/*, (GET)|(PATCH)|(DELETE)
+
+     p, admin, /users, POST
+     p, admin, /users/*, (GET)|(PATCH)|(DELETE)
+
+     # Transitive groups
+     # =================
+     g, poweruser, user
+     g, admin, poweruser
+
+     # Users
+     # =====
+g, Victor, admin
+g, Jack, poweruser
+     g, Hugo, user
+     */
+    await e.addPolicy('user', '/allowlist', 'GET');
+    await e.addPolicy('user', '/users', 'GET');
+    await e.addPolicy('user', '/users/info', 'GET');
+    await e.addPolicy('poweruser', '/allowlist', 'POST');
+    await e.addPolicy('poweruser', '/allowlist/*', '(GET)|(PATCH)|(DELETE)');
+    await e.addPolicy('admin', '/users', 'POST');
+    await e.addPolicy('admin', '/users/*', '(GET)|(PATCH)|(DELETE)');
+    await e.addGroupingPolicy('poweruser', 'user');
+    await e.addGroupingPolicy('admin', 'poweruser');
+    await e.addGroupingPolicy('Andi', 'admin');
+    await e.addGroupingPolicy('Jonas', 'poweruser');
+    await e.addGroupingPolicy('Hugo', 'user');
+
+    console.log(`e.getAllActions() =`, await e.getAllActions())
+
+    initEnforcer.enforcer = e;
     console.log('Enforcer initialized');
   }
   return initEnforcer.enforcer;
